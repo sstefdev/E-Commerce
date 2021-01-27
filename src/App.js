ed2afe67adfbc5f35d50ace9c94c53ded7040c74
sstefdev <sstefandev@gmail.com>
2021-01-26 16:04:04 -0800

    Added header component and styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/pages/homepage/homepage.copmonents";
 import ShopPage from "./components/pages/shop/shop.component";
+import Header from "./components/header/haeder.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
