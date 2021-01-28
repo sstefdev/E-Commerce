7c64eb455de6f174f19eac8163321fd98d9ea1f6
sstefdev <sstefandev@gmail.com>
2021-01-28 05:39:44 +0100

    Implemented firebase utils, included ability to store users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+// import { userRef } from "react";
 
 const config = {
   apiKey: "AIzaSyCKEPLj8tFPM4H9SQc1OJvYoyDzsNo13Fs",
@@ -10,6 +11,34 @@ const config = {
   messagingSenderId: "463004581040",
   appId: "1:463004581040:web:8d274ec6869ec245838f75",
   measurementId: "G-MF0E4C41RD",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
